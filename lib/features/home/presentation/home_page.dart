a07133915e27bfc5b56693184de215ed47f39255
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-04-19 16:23:17 +0900

    padding追加

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -18,64 +18,67 @@ class HomePage extends ConsumerWidget {
           child: Center(
             child: SizedBox(
               width: 400,
-              child: Column(
-                children: [
-                  const SizedBox(height: 32),
-                  SizedBox(
-                    height: 320,
-                    child: Image.asset('images/icon.png'),
-                  ),
-                  const Text(
-                    'Anaba',
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w200,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 32),
+                    SizedBox(
+                      height: 320,
+                      child: Image.asset('images/icon.png'),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    '「知る人ぞ売る」',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
+                    const Text(
+                      'Anaba',
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w200,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    '穴場販売プラットフォーム',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
+                    const SizedBox(height: 16),
+                    const Text(
+                      '「知る人ぞ売る」',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 32),
-                  if (user == null)
-                    ElevatedButton(
-                      onPressed: () {
-                        AuthDialog.show(context);
-                      },
-                      child: const Text('事前登録'),
-                    )
-                  else
-                    Text(
-                      '${user.displayName ?? ''}さん\n事前登録ありがとうございます。\nサービス開始の際はメールにて連絡いたします。',
-                      textAlign: TextAlign.center,
+                    const SizedBox(height: 8),
+                    const Text(
+                      '穴場販売プラットフォーム',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  const SizedBox(height: 32),
-                  const Text(
-                    '雑誌やサイトに載ったランキングでは満足できない。平均化された情報はいらない、誰かの熱狂的なおすすめが知りたい。そんなちょっと「あまのじゃく」なあなたに向けたサービスを開発中。',
-                    style: TextStyle(
-                      fontSize: 16,
+                    const SizedBox(height: 32),
+                    if (user == null)
+                      ElevatedButton(
+                        onPressed: () {
+                          AuthDialog.show(context);
+                        },
+                        child: const Text('事前登録'),
+                      )
+                    else
+                      Text(
+                        '${user.displayName ?? ''}さん\n事前登録ありがとうございます。\nサービス開始の際はメールにて連絡いたします。',
+                        textAlign: TextAlign.center,
+                      ),
+                    const SizedBox(height: 32),
+                    const Text(
+                      '雑誌やサイトに載ったランキングでは満足できない。平均化された情報はいらない、誰かの熱狂的なおすすめが知りたい。そんなちょっと「あまのじゃく」なあなたに向けたサービスを開発中。',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 32),
-                  const Text(
-                    'Anabaでは誰かがおすすめする穴場情報を売り買いすることができます。大切な人とのデート。年に一度の旅行。絶対に楽しみ切りたいイベントに、Anabaが助けになります。',
-                    style: TextStyle(
-                      fontSize: 16,
+                    const SizedBox(height: 32),
+                    const Text(
+                      'Anabaでは誰かがおすすめする穴場情報を売り買いすることができます。大切な人とのデート。年に一度の旅行。絶対に楽しみ切りたいイベントに、Anabaが助けになります。',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
